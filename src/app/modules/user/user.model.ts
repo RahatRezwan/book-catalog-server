89e7f6624aec96463cf3108232b1677680f69535
RahatRezwan <rahat15-10971@diu.edu.bd>
2023-08-30 07:39:51 +0600

    [DONE] Complete user model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from 'mongoose';
+import { IUser, UserModel } from './user.interface';
+
+const UserSchema = new Schema<IUser, UserModel>(
+   {
+      name: { type: String, required: true },
+      email: { type: String, required: true, unique: true },
+      password: { type: String, required: true },
+   },
+   { timestamps: true },
+);
+
+export const User = model<IUser, UserModel>('User', UserSchema);
